c06a5c1892c41d7b2edd8079c13b4c2be49f6e40
kaluojushi <694802801@qq.com>
2022-11-01 23:14:20 +0800

    ⌨ 添加 demo-1 组件

---
+++ docs/.vuepress/components/demo-1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <el-button type="primary" @click="handleClick">Click me</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "demo-1",
+  data() {
+    return {
+      value: "Hello VuePress"
+    };
+  },
+  methods: {
+    handleClick() {
+      alert(this.value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
